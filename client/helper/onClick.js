1a7d3b02d90d85ccfeebbd4083ab889cc307d206
Owen Liu <121781906+owenliliu@users.noreply.github.com>
2024-06-05 18:23:06 -0400

    rook bingchilling (#10)
    * rook bingchilling
    
    * rook final
    
    ---------
    
    Co-authored-by: J.Stephen <j.stephen.huang.developer@gmail.com>

--- client/helper/onClick.js
+++ client/helper/onClick.js
@@ -1,87 +1,81 @@
-import { rotate } from "../board/rotateBoard.js"
-import { getLegalMoves } from "./getLegalMoves.js"
+import { rotate } from "../board/rotateBoard.js";
+import { getLegalMoves } from "./getLegalMoves.js";
 
-let focused = []  // contains selected square element, or empty if nothing selected
-
+let focused = []; // contains selected square element, or empty if nothing selected
 
 // selecting a square
-function selectSquare (targetSquare) {
-    if (targetSquare.innerHTML === '') return  // if square is empty: return
+function selectSquare(targetSquare) {
+  if (targetSquare.innerHTML === "") return; // if square is empty: return
 
-    console.log(getLegalMoves(targetSquare))
-        
-    focused.push(targetSquare)
-    targetSquare.classList.add("selected")
+  console.log(getLegalMoves(targetSquare));
+
+  focused.push(targetSquare);
+  targetSquare.classList.add("selected");
 }
 
 // moving a piece
-function movePiece (focusedSquare, targetSquare){
-    
-    if (focusedSquare.id === targetSquare.id) {  // if user selected the same square, unselect it
-        focusedSquare.classList.remove("selected") 
-        focused.pop()
-        
-    } else {    // if user valid target square
-        
-        // check if the move is legal
-        const legalMoves = getLegalMoves(focusedSquare)
+function movePiece(focusedSquare, targetSquare) {
+  if (focusedSquare.id === targetSquare.id) {
+    // if user selected the same square, unselect it
+    focusedSquare.classList.remove("selected");
+    focused.pop();
+  } else {
+    // if user valid target square
 
+    // check if the move is legal
+    const legalMoves = getLegalMoves(focusedSquare);
 
-        if (!legalMoves.includes(targetSquare.id)) {   // if not a legal move
-            if (targetSquare.innerHTML === "") {          // empty square
-                unselectSquare(focusedSquare)
-            } else {
-                unselectSquare(focusedSquare)
-                selectSquare(targetSquare)
-            }
-        } else {
-            targetSquare.innerHTML = focusedSquare.innerHTML // piece moves to target square
-            focusedSquare.innerHTML = ""    // remove piece from old square
-            focused.push(targetSquare)
-            targetSquare.classList.add("selected")
+    if (!legalMoves.includes(targetSquare.id)) {
+      // if not a legal move
+      if (targetSquare.innerHTML === "") {
+        // empty square
+        unselectSquare(focusedSquare);
+      } else {
+        unselectSquare(focusedSquare);
+        selectSquare(targetSquare);
+      }
+    } else {
+      targetSquare.innerHTML = focusedSquare.innerHTML; // piece moves to target square
+      focusedSquare.innerHTML = ""; // remove piece from old square
+      focused.push(targetSquare);
+      targetSquare.classList.add("selected");
 
-            rotate()
-
-        }
-
+      // rotate()
     }
+  }
 }
 
-
-function unselectSquare (focusedSquare){
-    focusedSquare.classList.remove("selected")
-    focused = []
+function unselectSquare(focusedSquare) {
+  focusedSquare.classList.remove("selected");
+  focused = [];
 }
 
-export function onClick (event) {
-    const targetSquare = event.target // new selected square
+export function onClick(event) {
+  const targetSquare = event.target; // new selected square
 
-    // if no square selected
-    if (focused.length === 0) {
-      selectSquare(targetSquare)
+  // if no square selected
+  if (focused.length === 0) {
+    selectSquare(targetSquare);
 
-        // display legal squares
+    // display legal squares
+  }
 
-    }
-    
-    // if there is already a selected square
-    else if (focused.length === 1) {   
-        const focusedSquare = focused[0]  // our old square
+  // if there is already a selected square
+  else if (focused.length === 1) {
+    const focusedSquare = focused[0]; // our old square
 
+    movePiece(focusedSquare, targetSquare);
+  }
 
-        movePiece(focusedSquare, targetSquare)
-    }
+  // if there are two squares selected:
+  else if (focused.length === 2) {
+    // unselect old squares
+    const oldFocusedSquare = focused[0];
+    const oldSelectedSquare = focused[1];
+    unselectSquare(oldFocusedSquare);
+    unselectSquare(oldSelectedSquare);
 
-    // if there are two squares selected: 
-    else if (focused.length === 2) {  // unselect old squares
-        const oldFocusedSquare = focused[0]
-        const oldSelectedSquare = focused[1]
-        unselectSquare(oldFocusedSquare)
-        unselectSquare(oldSelectedSquare)
-
-
-        // select new square only if square has piece
-        selectSquare(targetSquare)
-        
-    }
+    // select new square only if square has piece
+    selectSquare(targetSquare);
+  }
 }
