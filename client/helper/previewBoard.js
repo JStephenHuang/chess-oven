bcae0ffc638f98ba5e44050f7bac48bf6136bb4c
J.Stephen <j.stephen.huang.developer@gmail.com>
2024-06-08 16:41:22 -0400

    on check

--- client/helper/previewBoard.js
+++ client/helper/previewBoard.js
@@ -3,8 +3,6 @@ import { getPiecesPosition } from "./getPiecesPosition.js"
 export function previewBoard(initial, start, end) {
     const board = getPiecesPosition().reverse()
 
-    console.log(`start: ${start} end: ${end}`)
-
     board[end[1]][end[0]] = initial
     board[start[1]][start[0]] = ""
 
