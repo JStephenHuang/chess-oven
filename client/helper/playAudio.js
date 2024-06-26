acc16dccb7690e76ad9c16b083fc8807ac0941d7
J.Stephen <j.stephen.huang.developer@gmail.com>
2024-06-26 15:03:50 -0400

    final

--- client/helper/playAudio.js
+++ client/helper/playAudio.js
@@ -1,26 +1,25 @@
 export function playAudio(moveType) {
+  const moveSound = new Audio("./assets/audio/move-self.mp3");
+  const captureSound = new Audio("./assets/audio/capture.mp3");
+  const checkSound = new Audio("./assets/audio/move-check.mp3");
+  const gameEndSound = new Audio("./assets/audio/game-end.mp3");
+  const castleSound = new Audio("./assets/audio/castle.mp3");
 
-  const moveSound = new Audio('/client/assets/audio/move-self.mp3');
-  const captureSound = new Audio('/client/assets/audio/capture.mp3');
-  const checkSound = new Audio('/client/assets/audio/move-check.mp3');
-  const gameEndSound = new Audio('/client/assets/audio/game-end.mp3');
-  const castleSound = new Audio('/client/assets/audio/castle.mp3');
-
-  switch(moveType) {
-    case 'move':
-        moveSound.play();
-        break;
-    case 'check':
-        checkSound.play();
-        break;
-    case 'castle':
-        castleSound.play();
-        break;
+  switch (moveType) {
+    case "move":
+      moveSound.play();
+      break;
+    case "check":
+      checkSound.play();
+      break;
+    case "castle":
+      castleSound.play();
+      break;
     case "checkmate" || "stalemate":
-        gameEndSound.play()
-        break;
+      gameEndSound.play();
+      break;
     default:
-        captureSound.play();
-        break;
+      captureSound.play();
+      break;
   }
 }
